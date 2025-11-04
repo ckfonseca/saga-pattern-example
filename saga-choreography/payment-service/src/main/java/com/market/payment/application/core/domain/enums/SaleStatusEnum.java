2112b9f269d013b4250d6f76f4a8d7e8f3d7a2c8
Cristiano Fonseca <cristiano_fonseca@apple.com>
2025-11-04 18:53:42 -0300

    The variable type was changed from primitive to wrapper type .

--- saga-choreography/payment-service/src/main/java/com/market/payment/application/core/domain/enums/SaleStatusEnum.java
+++ saga-choreography/payment-service/src/main/java/com/market/payment/application/core/domain/enums/SaleStatusEnum.java
@@ -11,15 +11,15 @@ public enum SaleStatusEnum {
     CANCELED(3);
 
     @Getter
-    private final int id;
+    private final Integer id;
 
-    SaleStatusEnum(int id) {
+    SaleStatusEnum(Integer id) {
         this.id = id;
     }
 
-    public static SaleStatusEnum toEnum(int id) {
+    public static SaleStatusEnum toEnum(Integer id) {
         return Arrays.stream(values()).filter(
-                        saleStatusEnum -> saleStatusEnum.getId() == id).
+                        saleStatusEnum -> saleStatusEnum.getId().equals(id)).
                 findFirst().orElseThrow(
                         () ->  new IllegalArgumentException(String.format("The id %d is invalid.", id))
                 );
