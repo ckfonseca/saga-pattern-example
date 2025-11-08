2730b2942ffb67ca516ed639b2056a2122e93551
Cristiano Fonseca <cristiano_fonseca@apple.com>
2025-11-08 19:24:17 -0300

    created_at and updated_at fields were added and inserts were included.

--- docker/database/init-user-db.sql
+++ docker/database/init-user-db.sql
@@ -4,8 +4,10 @@ CREATE TABLE `users` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `balance` decimal(38,2) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT IGNORE INTO users (id, name, balance) VALUES (1, 'Cristiano Fonseca', 1000.00);
-INSERT IGNORE INTO users (id, name, balance) VALUES (2, 'Rodrigo Brayner', 500.00);
+INSERT IGNORE INTO users (id, name, balance, created_at) VALUES (1, 'Cristiano Fonseca', 1000.00, NOW());
+INSERT IGNORE INTO users (id, name, balance, created_at) VALUES (2, 'Rodrigo Brayner', 500.00, NOW());
