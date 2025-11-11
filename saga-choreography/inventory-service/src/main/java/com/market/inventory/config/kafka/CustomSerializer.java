94d5a04cdc5dda351ecac8429eb5f72ee267a937
Cristiano Fonseca <cristiano_fonseca@apple.com>
2025-11-10 21:19:14 -0300

    Added source code to disable serialization of DateTime fields as Timestamp.

--- saga-choreography/inventory-service/src/main/java/com/market/inventory/config/kafka/CustomSerializer.java
+++ saga-choreography/inventory-service/src/main/java/com/market/inventory/config/kafka/CustomSerializer.java
@@ -1,23 +1,27 @@
 package com.market.inventory.config.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.market.inventory.adapters.out.message.SaleMessage;
-import lombok.RequiredArgsConstructor;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Serializer;
 
 public class CustomSerializer implements Serializer<SaleMessage> {
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Override
-    public byte[] serialize(String s, SaleMessage saleMessage) {
-        try{
-            if(saleMessage == null) {
-                return null;
-            }
-            return this.objectMapper.writeValueAsBytes(saleMessage);
-        } catch (Exception e) {
-            throw new SerializationException("Error when serializing SaleMessage to byte[]");
-        }
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  @Override
+  public byte[] serialize(String s, SaleMessage saleMessage) {
+    this.objectMapper.registerModule(new JavaTimeModule());
+    this.objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    try {
+      if (saleMessage == null) {
+        return new byte[0];
+      }
+      return this.objectMapper.writeValueAsBytes(saleMessage);
+    } catch (Exception e) {
+      throw new SerializationException("Error when serializing SaleMessage to byte[]");
     }
+  }
 }
