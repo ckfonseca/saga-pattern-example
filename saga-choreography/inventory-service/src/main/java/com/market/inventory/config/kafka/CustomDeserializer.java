987696de0ec899aed7837945c2757ebcd11dae0a
Cristiano Fonseca <cristiano_fonseca@apple.com>
2025-11-10 21:21:55 -0300

    Added source code to register the JavaTimeModule to assist in deserializing Timestamp fields.

--- saga-choreography/inventory-service/src/main/java/com/market/inventory/config/kafka/CustomDeserializer.java
+++ saga-choreography/inventory-service/src/main/java/com/market/inventory/config/kafka/CustomDeserializer.java
@@ -1,26 +1,29 @@
 package com.market.inventory.config.kafka;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.market.inventory.adapters.out.message.SaleMessage;
-import java.nio.charset.StandardCharsets;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Deserializer;
 
 public class CustomDeserializer implements Deserializer<SaleMessage> {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+  private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Override
-    public SaleMessage deserialize(String topic, byte[] data) {
-        try {
-            if(data == null) {
-                return null;
-            }
-            var src = new String(data, UTF_8);
-            return this.objectMapper.readValue(src, SaleMessage.class);
-        } catch (Exception e) {
-            throw new SerializationException("Error when deserializing byte[] to SaleMessage");
-        }
+  @Override
+  public SaleMessage deserialize(String topic, byte[] data) {
+    this.objectMapper.registerModule(new JavaTimeModule());
+    try {
+      if (data == null || data.length == 0) {
+        return null;
+      }
+      var src = new String(data, UTF_8);
+      return this.objectMapper.readValue(src, SaleMessage.class);
+    } catch (Exception e) {
+      throw new SerializationException(
+          "Error deserializing byte[] to SaleMessage in inventory microsservice");
     }
+  }
 }
