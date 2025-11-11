a60bf4f2602f95679ed03cacc2804ca16a701c93
Cristiano Fonseca <cristiano_fonseca@apple.com>
2025-11-10 21:27:56 -0300

    The class name has been changed.

--- saga-choreography/sale-service/src/main/java/com/market/sale/adapters/in/controller/mapper/SaleRequestMapper.java
+++ saga-choreography/sale-service/src/main/java/com/market/sale/adapters/in/controller/mapper/SaleRequestMapper.java
@@ -1,11 +1,11 @@
 package com.market.sale.adapters.in.controller.mapper;
 
-import com.market.sale.adapters.in.controller.request.SaleRequest;
+import com.market.sale.adapters.in.controller.dto.SaleRequestDTO;
 import com.market.sale.application.core.domain.Sale;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface SaleRequestMapper {
 
-  Sale saleRequestToSale(SaleRequest saleRequest);
+  Sale saleRequestDTOToSale(SaleRequestDTO saleRequestDTO);
 }
