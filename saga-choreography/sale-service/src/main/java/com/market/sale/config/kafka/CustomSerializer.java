acb0bb3d72777f57ea687b62ab30fa56b041a263
Cristiano Fonseca <cristiano_fonseca@apple.com>
2025-11-10 21:19:14 -0300

    Added source code to disable serialization of DateTime fields as Timestamp.

--- saga-choreography/sale-service/src/main/java/com/market/sale/config/kafka/CustomSerializer.java
+++ saga-choreography/sale-service/src/main/java/com/market/sale/config/kafka/CustomSerializer.java
@@ -1,6 +1,8 @@
 package com.market.sale.config.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.market.sale.adapters.out.message.SaleMessage;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Serializer;
@@ -11,9 +13,11 @@ public class CustomSerializer implements Serializer<SaleMessage> {
 
   @Override
   public byte[] serialize(String s, SaleMessage saleMessage) {
+    this.objectMapper.registerModule(new JavaTimeModule());
+    this.objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     try {
       if (saleMessage == null) {
-        return null;
+        return new byte[0];
       }
       return this.objectMapper.writeValueAsBytes(saleMessage);
     } catch (Exception e) {
